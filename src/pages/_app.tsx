140849220a14cc5ef7d4ae527a73578658872ca5
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-02-02 17:32:26 +0300

    update app component to render layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,27 @@
-import { type AppType } from "next/app";
-
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
+import type { AppProps, AppType } from "next/app";
 import { api } from "../utils/api";
-
+import Layout from "../components/Layout";
 import "../styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const MyApp: AppType<object> = ({
+  Component,
+  pageProps,
+}: AppPropsWithLayout) => {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>);
+  const pageWithLayout = getLayout(<Component {...pageProps} />) as JSX.Element;
+
+  return <>{pageWithLayout}</>;
 };
 
 export default api.withTRPC(MyApp);
