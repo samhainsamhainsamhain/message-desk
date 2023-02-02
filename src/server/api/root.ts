4b5f0cdd6255412a555edb3bf4c133d65627542a
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-02-02 17:33:28 +0300

    add apis to trpc router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,11 +1,16 @@
 import { createTRPCRouter } from "./trpc";
+import { messagesRouter } from "./routers/messages";
+import { numbersRouter } from "./routers/numbers";
 
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here
  */
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  messages: messagesRouter,
+  numbers: numbersRouter,
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
